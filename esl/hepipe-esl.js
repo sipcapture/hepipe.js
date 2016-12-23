e554692f8b9a07b12e8cbe86e8ad40de0f6de4fa
Giacomo Vacca <giacomo.vacca@gmail.com>
2016-12-23 14:56:03 +0100

    Start modularizing hepipe-esl

--- esl/hepipe-esl.js
+++ esl/hepipe-esl.js
@@ -9,17 +9,15 @@ console.log("HEPIPE-ESL v"+version+" (http://sipcapture.org)");
 console.log("Press CTRL-C to Exit...");
 
 /* Requires */
+var esl_client = require('./esl-client.js');
 
-var esl = require('modesl');
+
 var HEPjs = require('hep-js');
 var dgram = require('dgram');
 var socket = dgram.createSocket("udp4");
 
-var dirty = require('dirty');
-var db = dirty('uuid.db');
 
 /* Default Config */
-
 var _config_ = { 
   HEP_SERVER: '127.0.0.1', 
   HEP_PORT: 9060, 
@@ -34,7 +32,6 @@ var debug = false;
 var report_call_events = false;
 var report_rtcp_events = false;
 var report_qos_events = false;
-log = true;
 var exit = false;
 var stats = {rcvd: 0, parsed: 0, hepsent: 0, err: 0, heperr: 0 }; 
 
@@ -118,255 +115,10 @@ var preHep = function(message) {
   sendHEP3(msg,rcinfo);	
 };
 
-/* FS ESL Handler */
-var eslWaitTime = 60000;
 
-function fsConnect() {
-  eslConn = new esl.Connection(_config_.ESL_SERVER, _config_.ESL_PORT, _config_.ESL_PASS)
-    .on("error", function (error) {
-      console.log('ESL Connection Error ' + JSON.stringify(error));
-      setTimeout(fsConnect, eslWaitTime);
-    }).on("esl::end", function () {
-      console.log('ESL Connection Ended');
-      setTimeout(fsConnect, eslWaitTime);
-    }).on("esl::ready", function () {
-      // Subscribe to events
-      eslConn.events('json' , 'ALL', function() {
-      //eslConn.events('json' , 'CHANNEL_DESTROY', function() {
-        console.log('ESL ready - subscribed to receive events.');
-      });
-    }).on("esl::event::**", function (e, headers, body) {
-      // do stuff
-      if (debug) console.log('Event: ' + e.getHeader('Event-Name'));
-      if (debug) console.log('Unique-ID: ' + e.getHeader('Unique-ID'));
-      // No CID no Party! Check B-Leg, A-Leg and Included Header in sequence, Skip if none available
-      // if(!e.getHeader('variable_sip_call_id')) { 
-      // if (debug) console.log('Missing CID!');
+esl_client.init(_config_.HEP_ID, _config_.HEP_PASS, report_call_events, report_rtcp_events, report_qos_events);
+esl_client.connect(_config_.ESL_SERVER, _config_.ESL_PORT, _config_.ESL_PASS, preHep);
 
-      if (db) {
-        if (db.get(e.getHeader('Other-Leg-Unique-ID'))) {
-          if (debug) console.log('FOUND B-LEG!', db.get(e.getHeader('Other-Leg-Unique-ID')).cid);
-          var xcid = db.get(e.getHeader('Other-Leg-Unique-ID')).cid;
-        } else if (db.get(e.getHeader('Unique-ID'))) {
-          if (debug) console.log('FOUND!', db.get(e.getHeader('Unique-ID')).cid);
-          var xcid = db.get(e.getHeader('Unique-ID')).cid;
-        } else { 
-          if (debug) console.log('DEFAULT!', db.get(e.getHeader('variable_sip_call_id')));
-          var xcid = e.getHeader('variable_sip_call_id'); 
-        }
-      } else { var xcid = e.getHeader('variable_sip_call_id'); }
-      
-      /* EVENT LOGGER */
-      if (log) {
-        var payload = e.getHeader('Event-Date-Local') + ': ';
-        // Log Events to HEP Message
-        if(e.getHeader('Event-Name')) {
-          if(e.getHeader('Event-Name') == 'CHANNEL_CREATE') {
-            if(e.getHeader('Call-Direction') == 'inbound'){
-              // Inbound Call
-              payload +=  'RINGING; ';
-              payload +=  e.getHeader('Call-Direction') + '; ';
-              payload +=  e.getHeader('Caller-Caller-ID-Number') + '; ';
-              payload +=  e.getHeader('Caller-Destination-Number') + '; ';
-              payload +=  e.getHeader('Unique-ID') + '; ';
-              // payload +=  e.getHeader('') + '; ';
-            } else {
-              // Outbound Call
-              payload +=  'RINGING; ';
-              payload +=  e.getHeader('Call-Direction') + '; ';
-              payload +=  e.getHeader('Caller-Callee-ID-Number') + '; ';
-              payload +=  e.getHeader('Caller-Caller-ID-Number') + '; ';
-              payload +=  e.getHeader('Unique-ID') + '; ';
-              // payload +=  e.getHeader('') + '; ';
-            }
-
-            db.set(e.getHeader('Unique-ID'), {cid: e.getHeader('variable_sip_call_id')}, function() {
-              if (debug) console.log('Session init saved!');
-            });
-          } else if(e.getHeader('Event-Name') == 'CHANNEL_ANSWER') {
-            if(e.getHeader('Call-Direction') == 'inbound'){
-              // Inbound Call
-              payload += 'ANSWERED; ';
-              payload +=  e.getHeader('Caller-Caller-ID-Number') + '; ';
-              payload +=  e.getHeader('Unique-ID') + '; ';
-              // payload +=  e.getHeader('') + '; ';
-            } else {
-              // Outbound Call
-              payload += 'ANSWERED; ';
-              payload +=  e.getHeader('Caller-Callee-ID-Number') + '; ';
-              payload +=  e.getHeader('Unique-ID') + '; ';
-              // payload +=  e.getHeader('') + '; ';
-            }
-
-            db.set(e.getHeader('Unique-ID'), {cid: e.getHeader('variable_sip_call_id')}, function(e) {
-              if (debug) console.log('Session answer saved!' );
-            });
-          } else if(e.getHeader('Event-Name') == 'CHANNEL_DESTROY') {
-            if(e.getHeader('Call-Direction') == 'inbound'){
-              // Inbound Call
-              payload += 'HANGUP; ';
-              payload +=  e.getHeader('Caller-Caller-ID-Number') + '; ';
-              payload +=  e.getHeader('Unique-ID') + '; ';
-              // payload +=  e.getHeader('') + '; ';
-            } else {
-              // Outbound Call
-              payload += 'HANGUP; ';
-              payload +=  e.getHeader('Caller-Callee-ID-Number') + '; ';
-              payload +=  e.getHeader('Unique-ID') + '; ';
-              // payload +=  e.getHeader('') + '; ';
-            }
-            
-            db.rm(e.getHeader('Unique-ID'), function(e) {
-              if (debug) console.log('Session UUID has been removed!');
-            });
-          } else if(e.getHeader('Event-Name') == 'CUSTOM') {
-            if(e.getHeader('Event-Subclass') === 'sofia::register'){
-              payload += 'REGISTER; ';
-              payload +=  e.getHeader('from-user') + '; ';
-              payload +=  e.getHeader('network-ip') + '; ';
-            } else if(e.getHeader('Event-Subclass') === 'sofia::unregister'){
-              payload += 'UNREGISTER; ';
-              payload +=  e.getHeader('from-user') + '; ';
-              payload +=  e.getHeader('network-ip') + '; ';
-            }
-          } else if(e.getHeader('Event-Name') == 'DTMF') {
-            if (debug) console.log('DTMF EVENT',e);
-            payload += e.getHeader('Event-Name') + ' DIGIT: '+e.getHeader('DTMF-Digit') + ' DURATION: '+e.getHeader('DTMF-Duration') + ' FROM:'+ e.getHeader('variable_sip_from_user') + ' TO: ' + e.getHeader('variable_sip_req_user');
-          } else { 
-            payload += e.getHeader('Event-Name') + '; ' + e.getHeader('Channel-Name') + ' (' + e.getHeader('Event-Calling-Function') + ')';
-          }
-
-          // Format HEP Header
-          if (report_call_events) {
-            var message = {
-              rcinfo: {
-                type: 'HEP',
-                version: 3,
-                payload_type: 'JSON',
-                captureId: _config_.HEP_ID,
-                capturePass: _config_.HEP_PASS,
-                ip_family: 2,
-                protocol: 17,
-                proto_type: 100,
-                srcIp: e.getHeader('FreeSWITCH-IPv4'),
-                dstIp: e.getHeader('FreeSWITCH-IPv4'),
-                srcPort: 0,
-                dstPort: 0,
-                correlation_id: xcid ? xcid : e.getHeader('variable_sip_call_id')
-              },
-              payload: payload
-            };
-            // Prepare for shipping!
-            preHep(message);
-          }
-        }
-      }
-
-      /* RTCP EMULATION */
-      if (report_rtcp_events) {
-        if(e.getHeader('Event-Name') == 'RECV_RTCP_MESSAGE') {
-          if (e.getHeader('Source0-SSRC')) {
-            if (debug) console.log('Processing RTCP Report...',e);
-            var message = {
-              rcinfo: {
-                type: 'HEP',
-                version: 3,
-                payload_type: 'JSON',
-                captureId: _config_.HEP_ID,
-                capturePass: _config_.HEP_PASS,
-                ip_family: 2,
-                protocol: 17,
-                proto_type: 5,
-                srcIp: e.getHeader('variable_local_media_ip') ? e.getHeader('variable_local_media_ip') : '127.0.0.1',
-                dstIp: e.getHeader('variable_remote_audio_ip_reported') ? e.getHeader('variable_remote_audio_ip_reported') : '127.0.0.1',
-                srcPort: parseInt(e.getHeader('variable_local_media_port')) ? parseInt(e.getHeader('variable_local_media_port')) : 0,
-                dstPort: parseInt(e.getHeader('variable_remote_media_port')) ? parseInt(e.getHeader('variable_remote_media_port')) : 0,
-                correlation_id: xcid ? xcid : db.get(e.getHeader('Unique-ID')).cid
-              },
-              // HEP Type 5
-              payload:  JSON.stringify({ 
-                "type":200,
-                "ssrc": e.getHeader('SSRC'),
-                "report_count": parseInt(e.getHeader('Event-Sequence')),
-                "report_blocks":[{
-                  "source_ssrc": e.getHeader('Source0-SSRC'),
-                  "fraction_lost": parseInt(e.getHeader('Source0-Fraction')),
-                  "packets_lost": parseInt(e.getHeader('Source0-Lost')),
-                  "highest_seq_no": parseInt(e.getHeader('Source0-Highest-Sequence-Number-Received')),
-                  "lsr": parseInt(e.getHeader('Source0-LSR')),
-                  "ia_jitter": parseFloat(e.getHeader('Source0-Jitter')),
-                  "dlsr": parseInt(e.getHeader('Source0-DLSR'))
-                }],
-                "sender_information":{
-                  "packets": parseInt(e.getHeader('Sender-Packet-Count')),
-                  "ntp_timestamp_sec": parseInt(e.getHeader('NTP-Most-Significant-Word')),
-                  "ntp_timestamp_usec": parseInt(e.getHeader('NTP-Least-Significant-Word')),
-                  "rtp_timestamp": parseInt(e.getHeader('RTP-Timestamp')),
-                  "octets": parseInt(e.getHeader('Octect-Packet-Count'))
-                }
-              })
-            };
-            // Prepare for shipping!
-            preHep(message);
-          }
-        }
-      }
-
-      /* CUSTOM HEP QoS REPORT */
-      if (report_qos_events) {
-        if(e.getHeader('Event-Name') == 'CHANNEL_DESTROY') {
-          if(e.getHeader('variable_rtp_use_codec_rate') && e.getHeader('variable_sip_call_id')) {
-            var message = {
-              rcinfo: {
-                type: 'HEP',
-                version: 3,
-                payload_type: 'JSON',
-                captureId: _config_.HEP_ID,
-                capturePass: _config_.HEP_PASS,
-                ip_family: 2,
-                protocol: 17,
-                proto_type: 32,
-                mos: -1,
-                srcIp: e.getHeader('variable_local_media_ip') ?  e.getHeader('variable_local_media_ip') : '127.0.0.1',
-                dstIp: e.getHeader('variable_remote_audio_ip_reported') ? e.getHeader('variable_remote_audio_ip_reported') : '127.0.0.1',
-                srcPort: parseInt(e.getHeader('variable_local_media_port')) ? parseInt(e.getHeader('variable_local_media_port')) : 0,
-                dstPort: parseInt(e.getHeader('variable_remote_media_port')) ? parseInt(e.getHeader('variable_remote_media_port')) : 0,
-                correlation_id: xcid ? xcid : e.getHeader('variable_sip_call_id')
-              },
-              // HEP Type 33
-              payload:  JSON.stringify({ 
-                "CORRELATION_ID": xcid ? xcid : e.getHeader('variable_sip_call_id'),
-                "RTP_SIP_CALL_ID": xcid ? xcid : e.getHeader('variable_sip_call_id'),
-                "JITTER": (parseInt(e.getHeader('variable_rtp_audio_in_jitter_max_variance')) + parseInt(e.getHeader('variable_rtp_audio_in_jitter_max_variance')))/2,
-                "REPORT_TS": parseInt(e.getHeader('Event-Date-Timestamp')),
-                "TL_BYTE": parseInt(e.getHeader('variable_rtp_audio_in_media_bytes'))+parseInt(e.getHeader('variable_rtp_audio_out_media_bytes')),
-                "TOTAL_PK": parseInt(e.getHeader('variable_rtp_audio_in_packet_count'))+parseInt(e.getHeader('variable_rtp_audio_out_packet_count')),
-                "PACKET_LOSS": parseInt(e.getHeader('variable_rtp_audio_in_skip_packet_count'))+parseInt(e.getHeader('variable_rtp_audio_out_skip_packet_count')),
-                "MAX_JITTER": parseFloat(e.getHeader('variable_rtp_audio_in_jitter_max_variance')),
-                "MIN_JITTER": parseFloat(e.getHeader('variable_rtp_audio_in_jitter_min_variance')),
-                "DELTA": parseFloat(e.getHeader('variable_rtp_audio_in_mean_interval')),
-                "MOS": parseFloat(e.getHeader('variable_rtp_audio_in_mos')),
-                "SRC_IP": e.getHeader('variable_advertised_media_ip'), 
-                "SRC_PORT": parseInt(e.getHeader('variable_local_media_port')), 
-                "DST_IP": e.getHeader('variable_remote_media_ip'),
-                "DST_PORT": parseInt(e.getHeader('variable_remote_media_port')),
-                "CODEC_PT": parseInt(e.getHeader('variable_rtp_audio_recv_pt')), 
-                "PTIME": parseInt(e.getHeader('variable_rtp_use_codec_ptime')),
-                "CLOCK": parseInt(e.getHeader('variable_rtp_use_codec_rate')),
-                "CODEC_NAME": e.getHeader('variable_rtp_use_codec_name'),
-                "TYPE": e.getHeader('Event-Name')
-              })
-            };
-            // Prepare for shipping!
-            preHep(message);
-          }
-        }
-      }
-    });
-}
-
-/* Start Loop */
-fsConnect();
  /* Exit */
 process.on('SIGINT', function() {
   console.log();
