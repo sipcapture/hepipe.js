b4209de737300f4ffbb1ffd8e4b417b084653c91
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2017-01-28 13:51:34 +0100

    fix declaration

--- hepipe.js
+++ hepipe.js
@@ -32,7 +32,7 @@ if (config.esl_config) {
   if (config.esl_config.isArray) {
     var esl_client = [];
     config.esl_config.forEach(function(esl,index) {
-        var esl_client[index] = require('./esl-client.js');
+        esl_client[index] = require('./esl-client.js');
         esl_client[index].connect(esl, hep_client.preHep);
     })
   } else {
