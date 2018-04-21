4a9aa15d1811ffb3add19613abdefd38db0a83e1
Brad Gardner <bradgardner@sevenhillstechnology.com>
2018-04-21 11:38:55 -0400

    include media port and ip in cache and lookup for RTCP messages

--- esl-client.js
+++ esl-client.js
@@ -102,7 +102,13 @@ var eslConnect = function(host, port, pass, callback_preHep) {
               payload +=  e.getHeader('Unique-ID') + '; ';
             }
 
-            db.set(e.getHeader('Unique-ID'), {cid: e.getHeader('variable_sip_call_id')}, ttl);
+            db.set(e.getHeader('Unique-ID'), {
+              cid: e.getHeader('variable_sip_call_id'),
+              localMediaIp: e.getHeader('variable_local_media_ip'),
+              remoteMediaIp: e.getHeader('variable_remote_media_ip'),
+              localMediaPort: e.getHeader('variable_local_media_port'),
+              remoteMediaPort: e.getHeader('variable_remote_media_port')
+            }, ttl);
           } else if(e.getHeader('Event-Name') == 'CHANNEL_DESTROY') {
             if(e.getHeader('Call-Direction') == 'inbound'){
               payload += 'HANGUP; ';
@@ -163,6 +169,9 @@ var eslConnect = function(host, port, pass, callback_preHep) {
 };
 
 var getRTCPMessage = function(e, xcid, hep_id, hep_pass) {
+            console.log(JSON.stringify(e));
+            var call = db.get(e.getHeader('Unique-ID'));
+            
             var message = {
               rcinfo: {
                 type: 'HEP',
@@ -173,10 +182,10 @@ var getRTCPMessage = function(e, xcid, hep_id, hep_pass) {
                 ip_family: 2,
                 protocol: 17,
                 proto_type: 5,
-                srcIp: e.getHeader('variable_local_media_ip') ? e.getHeader('variable_local_media_ip') : '127.0.0.1',
-                dstIp: e.getHeader('variable_remote_audio_ip_reported') ? e.getHeader('variable_remote_audio_ip_reported') : '127.0.0.1',
-                srcPort: parseInt(e.getHeader('variable_local_media_port')) ? parseInt(e.getHeader('variable_local_media_port')) : 0,
-                dstPort: parseInt(e.getHeader('variable_remote_media_port')) ? parseInt(e.getHeader('variable_remote_media_port')) : 0,
+                srcIp: call.localMediaIp ? call.localMediaIp : '127.0.0.1',
+                dstIp: call.remoteMediaIp ? call.remoteMediaIp : '127.0.0.1',
+                srcPort: call.localMediaPort ? call.localMediaPort : 0,
+                dstPort: call.remoteMediaPort ? call.remoteMediaPort : 0,
                 correlation_id: xcid ? xcid : ''
               },
               payload:  JSON.stringify({ 
